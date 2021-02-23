250d3be04d1093d348a9ded594ee27a17d727014
Genesis Thomas <gthomas@perforce.com>
2021-02-23 12:01:06 +0530

    Update sample.js

--- Selenium/Javascript/protractor/sample.js
+++ Selenium/Javascript/protractor/sample.js
@@ -18,8 +18,7 @@ describe('Selenium NodeJS', () => {
         browser.reportingClient.stepStart('Select Perfecto');
         var q = element(by.xpath('(//*[text()="' + search + '"])[1]'));
         browser.wait(EC.elementToBeClickable(q), timeout);
-        browser.sleep(4000);
-        browser.executeScript("arguments[0].click()", q);
+        browser.actions().sendKeys(protractor.Key.ENTER).perform();
         browser.reportingClient.stepEnd();
 
         browser.reportingClient.stepStart('Navigate to Perfecto');
